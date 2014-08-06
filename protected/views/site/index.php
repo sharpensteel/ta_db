2d4504e44aae72e802ec8311084e9a02688e6e01
unknown <ss@gg.com>
2014-08-07 03:10:59 +0400

    mmm

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -12,5 +12,5 @@ $this->pageTitle=Yii::app()->name;
 <a href="<?=createUrl("attacks_table")?>">attacks table</a><br><br>
 
 <br>
-<span style="text-decoration:underline">Activity record by last 30 days:</span><br><br>
+<span style="text-decoration:underline">Players activity by last 30 days:</span><br><br>
 <? $this->widget('application.widgets_custom.Report_activity_widget',array('interval_days'=>30));

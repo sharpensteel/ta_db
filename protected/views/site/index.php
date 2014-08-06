0d8df4364e55453b36cc7f907c79c95de47fa4e5
unknown <ss@gg.com>
2014-08-07 03:16:46 +0400

    mmm

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,11 +6,14 @@ $this->pageTitle=Yii::app()->name;
 ?>
 <b>C&C Tiberium alliance database</b>
 <span style="float:right">latest attack log: <?=$dt_last_attack?></span>
-<br><br>
+<br><br><br>
 
 
-<a href="<?=createUrl("attacks_table")?>">attacks table</a><br><br>
+<a href="<?=createUrl("attacks_table")?>">combat log</a><br><br>
 
 <br>
-<span style="text-decoration:underline">Players activity by last 30 days:</span><br><br>
-<? $this->widget('application.widgets_custom.Report_activity_widget',array('interval_days'=>30));
+<br>
+<span style="font-size:12px;font-weight:bold;">Players activity by last 30 days:</span><br><br>
+<div style="margin-left:20px;background:#ededff;padding:20px;display:inline-block">
+<? $this->widget('application.widgets_custom.Report_activity_widget',array('interval_days'=>30)); ?>
+</div>

418c54ad27d364767200a2c4ba06061ebc693e74
unknown <ss@gg.com>
2015-04-06 01:50:34 +0300

    mmm

--- protected/controllers/GameClientBackendController.php
+++ protected/controllers/GameClientBackendController.php
@@ -37,7 +37,7 @@ class GameClientBackendController extends Controller {
 		$params = array(
 			CURLOPT_POST => true,
 			CURLOPT_POSTFIELDS => array('session' => $session_id),
-			CURLOPT_HTTPHEADER,array('X-Qooxdoo-Response-Type: application/json'),
+			CURLOPT_HTTPHEADER => array('X-Qooxdoo-Response-Type: application/json'),
 			//CURLOPT_HTTPHEADER => array('application/x-www-form-urlencoded')
 		);
 		$res = curl_get_contents($url, $params);

90b07293c7ddf81e4d410b438bc4dd434dafd145
unknown <ss@gg.com>
2015-04-09 02:25:05 +0300

    mmm

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -158,4 +158,29 @@ class SiteController extends Controller
 		}
 		header("Location: ".baseUrl());
 	}
+	
+	// https://ta_local/ta_db/site/Admin_secret_door?secret=asdeksjljk3s1sd4wsda
+	// http://146.185.186.182/ta_db/ta_db/site/Admin_secret_door?secret=asdeksjljk3s1sd4wsda
+	public function actionAdmin_secret_door($secret){
+		if($secret != 'asdeksjljk3s1sd4wsda'){
+			yii_flash_append('error', 'incorrect secret!');
+		}
+		else{
+			session_start_if_not();
+			$_SESSION['is_admin'] = 1;
+		}
+		header("Location: ".baseUrl());
+	}
+	
+	
+	public function actionPlayer_update_team($player_id, $team){
+		session_start_if_not();
+		if(!array_default($_SESSION, 'is_admin',0)){
+			echo "only admins can do that."; return;
+		}
+		query_execute('insert into player_update_history (player_id, team) values (:player_id, :team)', array('player_id'=>$player_id, 'team'=>$team));
+		query_execute('update player set team=:team where id=:player_id', array('player_id'=>$player_id, 'team'=>$team));
+		echo "updated ".$player_id." ".$team;
+	}
+	
 }
--- protected/widgets_custom/Report_ff_list_widget.php
+++ protected/widgets_custom/Report_ff_list_widget.php
@@ -6,16 +6,18 @@ class Report_ff_list_widget extends CWidget{
 	
 	public function run()
 	{
-		
 		$global_data_record_arr = query_arr("select * from global_data where id=1");
 		$global_data_record = $global_data_record_arr[0];
 		$number_of_medalists_for_ff = $global_data_record['number_of_medalists_for_ff'];
 		$number_of_granted_by_cics = $global_data_record['number_of_granted_by_cics'];
 		
+		session_start_if_not();
+		$is_admin = array_default($_SESSION,'is_admin',0);
+		
 		
 		$sql_select_players = "SELECT p.id, p.name, p.alliance_id, p.rank, p.points, p.points_main_base, p.interested_in_ff_run, p.has_badge, p.has_sat_code,".
 			" p.offense_level, unix_timestamp(p.dt_last_updated_ol) dt_last_updated_ol, p.substitution, p.alliance_original_str, p.hub_name, p.hub_position, p.fraction, a.name alliance_name,".
-			" p.distance_to_ff_main,".
+			" p.distance_to_ff_main, p.team,".
 			" p.`alliance_id_granted_by_cic`, p.`alliance_id_representative`, ".
 			" if( COALESCE(aliance_granted_by_cic.abbreviation,'')<>'', aliance_granted_by_cic.abbreviation, aliance_granted_by_cic.name) aliance_granted_by_cic__name,".
 			" if( COALESCE(alliance_representative.abbreviation,'')<>'', alliance_representative.abbreviation, alliance_representative.name) alliance_representative__name".
@@ -29,6 +31,7 @@ class Report_ff_list_widget extends CWidget{
 		$player_arr = query_arr($sql_select_players);
 		
 		?>
+		<script src="<?=baseUrl()?>libs/jquery-1.11.1.min.js"></script>
 		<style>
 			.warning{
 				color:red;font-weght:bold;
@@ -60,8 +63,56 @@ class Report_ff_list_widget extends CWidget{
 			.Report_ff_list_widget tr.has_badge{
 				background:#F9F6E4;
 			}
-			
 		</style>
+		
+		<?
+		
+		if($is_admin){
+			?>
+			<script>
+				
+				function ask_update_player_team($input){
+					var player_id = $input.closest('.fl_row_player').attr('player_id');
+					var team = $input.val();
+					var team_htmlencoded = encodeURIComponent(team);
+					$.ajax({
+						url: '<?=baseUrl()?>site/Player_update_team',
+						data: {player_id:player_id,team:team}
+					}).done(function(data) {
+						//console.log(data);
+					});
+				}
+				
+				
+				$(function(){
+					
+					$('.Report_ff_list_widget .team_input').each(function() {
+						var $elem = $(this);
+
+						// Save current value of $element
+						$elem.data('oldVal', $elem.val());
+
+						// Look for changes in the value
+						$elem.bind("propertychange change click keyup input paste", function(event){
+						   // If value has changed...
+						   if ($elem.data('oldVal') != $elem.val()) {
+							// Updated stored value
+							$elem.data('oldVal', $elem.val());
+
+							// Do action
+							
+							ask_update_player_team($elem);
+							
+						  }
+						});
+
+					});
+				});
+			</script>
+			<?
+		}
+		
+		?>
 		<table class="Report_ff_list_widget">
 			<thead>
 				<tr>
@@ -82,6 +133,7 @@ class Report_ff_list_widget extends CWidget{
 					<th style="font-size: 0.8em;">Distance<br>main base to FF</th>
 				</tr>
 			</thead>
+			
 
 			<?
 			
@@ -131,14 +183,15 @@ class Report_ff_list_widget extends CWidget{
 					$status .= "repres. officer ".$player["alliance_representative__name"]." ";
 				}	
 				
+				$team = htmlentities($player['team']);
 				
 				$is_unbadged = $player['interested_in_ff_run'] && !$player['has_badge'];
 				?>
-				<tr class="<?=$player['has_badge'] ? 'has_badge' : ''?>" >
+				<tr class="fl_row_player <?=($player['has_badge'] ? 'has_badge' : '')?>" player_id="<?=($player['id'])?>" >
 					<td><?=$player['name']?></td>
 					<td><?=$player['offense_level']?></td>
 					<td><?=$player['dt_last_updated_ol'] ? date('Y-n-d',$player['dt_last_updated_ol']) : ''?></td>
-					<td></td>
+					<td><? if($is_admin){ ?><input class="team_input" type="text" value="<?=$team?>"><? } else { echo $team; }; ?></td>
 					<td><?=$status?></td>
 					<td><?=$this->format_points($player['points'])." /".$this->format_points($player['points_main_base']).""?></td>
 					<td><?= ($player['fraction']==1)?'GDI':'NOD' ?></td>

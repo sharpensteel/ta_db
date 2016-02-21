2e34cabf29fe27cc7c5d2311b51be736388cd9da
Dmitry Sobchenko <sharpensteel@gmail.com>
2016-02-21 15:13:26 +0300

    mmm

---
+++ ta_stuff/index.php
@@ -0,0 +1,56 @@
+<?php
+?>
+
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+</head>
+<body style="line-height: 1.4;font-family: Tahoma;font-size: 13px;">
+<style>
+	.screen{
+		display: block;
+		max-width: 500px;
+		margin-top: 20px;
+		margin-bottom: 20px;
+	}
+
+</style>
+1. Open browser, open extension
+<a href="https://addons.mozilla.org/ru/firefox/addon/greasemonkey/" target="_blank">Greasemonkey</a> (for Firefox)
+or <a href="https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo" target="_blank">Tampermonkey</a> (for Chrome)<br>
+and install scripts:<br>
+1. <a href="Vortek Preload.user.js">Vortek Preload.user.js</a><br>
+2. <a href="TA stuff.user.js">TA stuff.user.js</a><br>
+“Vortek Preload” must be loaded earlier then “Ta stuff”<br>
+<br>
+after install, it will look like that:<br>
+<img class="screen" src="img/1.png"><br>
+<br>
+
+2. Open <a href="https://tiberium.mooo.com/ta_db/GameClientBackend/Staring_script?rnd=0.4766798347700387&_=1455324082055">link</a>. browser will show warning, it's normal.<br>
+<br>
+Click on link (1) and then on link (2) to proceed opening page
+<img class="screen" src="img/3.png">
+<br>
+After that, page with javascript souce will be shown<br>
+<img class="screen" src="img/4.png">
+<br>
+Nothing to do here, just keep this tab open.<br>
+<br>
+<br>
+3.Login to <a href="https://tiberiumalliances.com" target="_blank">tiberiumalliances.com</a> <br>
+there will be small bar. Click “expand”<br>
+<img class="screen" src="img/5.png"><br>
+Then click “Hacker attack report”<br>
+<img class="screen" src="img/6.png"><br>
+<br>
+Okey, final part! You can see list who is not injected yet<br>
+<br>
+You can click “refresh” to update report<br>
+Also you can click “collapse” to minimize panels<br>
+<img class="screen" src="img/7.png"><br>
+</body>
+</html>
+
+
+
